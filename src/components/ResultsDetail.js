50595efb9b4d8e39585cd653447a11b8a59b4409
robfoss <robfoss@rocketmail.com>
2021-03-30 09:59:31 -0400

    search and results screen

---
+++ src/components/ResultsDetail.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import { StyleSheet, Text, View, Image } from 'react-native'
+
+const ResultsDetail = ({ result }) => {
+    return (
+        <View style={styles.container}>
+            <Image
+                source={{ uri: result.image_url }}
+                style={styles.image}
+            />
+            <Text style={styles.name}>{result.name}</Text>
+            <Text>{result.rating} Stars, {result.review_count} Reviews</Text>
+        </View >
+    )
+}
+
+export default ResultsDetail
+
+const styles = StyleSheet.create({
+    container: {
+        marginLeft: 15,
+
+    },
+    image: {
+        width: 250,
+        height: 120,
+        borderRadius: 4,
+        marginBottom: 5,
+    },
+    name: {
+        fontWeight: 'bold',
+    }
+})
--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,26 +1,38 @@
-import React, { useState, useEffect } from 'react'
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react'
+import { StyleSheet, Text, View, ScrollView } from 'react-native';
 import SearchBar from '../components/SearchBar';
 import useResults from '../hooks/useResults';
+import ResultsList from '../components/ResultsList'
 
 
 const SearchScreen = () => {
     const [term, setTerm] = useState('');
     const [searchApi, results, errorMessage] = useResults();
 
+    const filterResultsByPrice = (price) => {
+        return results.filter(result => {
+            return result.price === price;
+        })
+    }
     return (
-        <View>
+        <>
             <SearchBar
                 term={term}
                 onTermChange={setTerm}
                 onTermSubmit={() => searchApi(term)}
             />
             {errorMessage ? <Text>{errorMessage}</Text> : null}
-            <Text>We have found {results.length} results</Text>
-        </View>
+            <ScrollView>
+                <ResultsList results={filterResultsByPrice('$')} title='Cost Effective' />
+                <ResultsList results={filterResultsByPrice('$$')} title='Bit Pricier' />
+                <ResultsList results={filterResultsByPrice('$$$')} title='Big Spender!' />
+            </ScrollView>
+        </>
     )
 }
 
 export default SearchScreen
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+
+})
